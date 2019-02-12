6f1778d2af84e2f0e7513a07bc37ff33d03986a8
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-02-12 19:42:54 +0100

    init

---
+++ src/es/studium/Aleatorios/Aleatorios.java
@@ -0,0 +1,11 @@
+package es.studium.Aleatorios;
+
+public class Aleatorios
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Número aleatorio: " + Math.random());
+		System.out.println("Otro número aleatorio: " + Math.random());
+	}
+
+}
